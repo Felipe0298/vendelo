8695e8bd7ec346880180f9ca5e0b428ad9e1cacc
felipe hoyos taborda <felipe02-98@hotmail.com>
2023-06-20 16:00:19 -0500

    Registro de usuarios

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source "https://rubygems.org"
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby "3.0.0"
+ruby "3.2.2"
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.5"
@@ -34,7 +34,7 @@ gem "jbuilder"
 # gem "kredis"
 
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
-# gem "bcrypt", "~> 3.1.7"
+gem 'bcrypt', '~> 3.1', '>= 3.1.16'
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
@@ -72,3 +72,6 @@ group :test do
 end
 
 gem "pg_search", "~> 2.3"
+
+gem 'did_you_mean', '~> 1.5', '>= 1.5.0'
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,8 @@
 Rails.application.routes.draw do
-  resources :categories, expect: :show
-    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :categories, except: :show
+  resources :products, path: '/'
 
-  # Defines the root path route ("/")
-  #get "/products/new", to: "products#new", as: :new_product
-  #get "/products" , to: "products#index"
-  #get "products/:id", to: "products#show", as: :product
-
-  #post "/products", to: "products#create"
-
-  #get "/products/:id/edit", to: "products#edit", as: :edit_product
-  #patch "/products/:id", to: "products#update"
-
-  #delete "/products/:id", to:"products#destroy"
-
-  resources :products, path:"/"
-  
-  #root "products#index"
+  namespace :authentication, path: '', as: '' do
+    resources :users, only: [:new, :create]
+  end
 end
