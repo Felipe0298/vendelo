8144293b29629dde9f4548dba63e16047f66f5af
felipe <felipe02-98@hotmail.com>
2023-06-26 13:35:23 -0500

    Stimulus JS

--- db/schema.rb
+++ db/schema.rb
@@ -43,7 +43,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_22_160556) do
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
